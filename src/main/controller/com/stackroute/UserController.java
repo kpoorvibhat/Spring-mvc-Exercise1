c7ba72864757fac31fd09e2b147d66974bd8d46d
PoorviBhat <kpoorvibhat@gmail.com>
2019-09-26 19:52:57 +0530

    initial commit

---
+++ src/main/controller/com/stackroute/UserController.java
@@ -0,0 +1,21 @@
+package com.stackroute;
+
+import com.stackroute.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class UserController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String displayName(Model model) {
+        User user = new User("Poorvi");
+        System.out.println(user.getName());
+        model.addAttribute("user", user.getName());
+        return "iindex";
+    }
+
+}
---
+++ target/SpringMVC/index.jsp
@@ -0,0 +1,6 @@
+<%@ page language = "java" contentType = "text/html"; charset= UTF-8 pageEncoding="UTF-8" isELIgnored="false" %>"
+<html>
+<body>
+<h2>Welcome ${user} to stackroute</h2>
+</body>
+</html>
